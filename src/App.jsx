003b7706495b0db0b0e797f5efd80d53f2541f85
MinhGiau <huynh.minhgiau.dev@gmail.com>
2026-03-13 15:06:00 +0700

    Config MUI Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <div>minhgiau </div>
+      <Typography variant="body2" color="text.secondary">Test Typo</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained" color="primary">Contained</Button>
       <Button variant="outlined">Outlined</Button>
